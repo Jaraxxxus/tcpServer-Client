c402291ab403c43f8f45822963a6e68dd3280909
Jaraxxxus <72243384+Jaraxxxus@users.noreply.github.com>
2024-03-27 13:24:29 +0700

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -9,16 +9,14 @@
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <server_ip> <server_port>" << std::endl;
-        //return 1;
+         throw std::runtime_error("few arguments");
     }
     int port = std::atoi(argv[2]);
     std::string ipAddress = argv[1];
 
-    //TcpClient client( 7070, "127.0.0.1" );
     TcpClient client( port, ipAddress );
 
     std::thread th(inputHandlerThread, std::ref(client));
-
     client.start();
 
     th.join();
